8f98d746e02373abf9a94f2a41282b12e9d2eebe
Goswami <681468@cognizant.com>
2018-12-10 15:45:03 +0530

    2nd angular assignment

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { RouterService } from '../services/router.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,37 @@ export class LoginComponent {
     username = new FormControl();
     password = new FormControl();
 
-    loginSubmit() {
+    submitMessage: string;
 
+    constructor(private routerSvc: RouterService,
+      private authSvc: AuthenticationService) {
+
+    }
+
+    loginSubmit() {
+      const credentials = {
+        'username': this.username.value,
+        'password': this.password.value,
+      };
+      const authObs = this.authSvc.authenticateUser(credentials);
+      authObs.subscribe(
+        resp => {
+          if (resp) {
+            this.routerSvc.routeToDashboard();
+            this.authSvc.setBearerToken(resp);
+          } else {
+            this.submitMessage = 'Unauthorized';
+          }
+        },
+        err => {
+          if (err.error) {
+            this.submitMessage = err.error.message;
+          } else {
+            this.submitMessage = err.message;
+          }
+        }
+      );
+
+      return authObs;
     }
 }
